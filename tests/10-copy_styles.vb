0d55412f975302ef6f94598f260bfdc615492df3
brrd <brrd@users.noreply.github.com>
2015-06-15 13:39:05 +0200

    Test 10: adding copyStylesOneByOne()

--- tests/10-copy_styles.vb
+++ tests/10-copy_styles.vb
@@ -21,9 +21,31 @@ Private Function copyStyles()
     Template:=BasePath
 End Function
 
+Private Function copyStylesOneByOne()
+    Set baseDocument = Documents.Open(FileName:=BasePath, Visible:=False)
+    For Each Style In baseDocument.Styles
+        If Style.BuiltIn = False Then
+             If Style.NameLocal = "auteur" Then
+             Application.OrganizerCopy source:=baseDocument.FullName, _
+             Destination:=GeneratorPath + "\test10.doc", _
+             Name:="auteur", _
+             Object:=wdOrganizerObjectStyles
+             End If
+        End If
+    Next Style
+    baseDocument.Close
+End Function
+
 Sub test()
     Call init
     Call createNewDoc
     Call copyStyles
+    ProcessedDoc.Save
+End Sub
+
+Sub test2()
+    Call init
+    Call createNewDoc
+    Call copyStylesOneByOne
     ProcessedDoc.Save
 End Sub
